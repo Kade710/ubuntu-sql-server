cf93b6baa83a802be36a9415473f2d08a89c0458
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-12 22:59:33 -0500

    added schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,24 @@
+-- =====================================================
+-- Ubuntu SQL Server
+-- Schema Definition
+-- =====================================================
+--
+-- Project: Ubuntu SQL Server
+-- Repository: github.com/Kade710/ubuntu-sql-server
+--
+-- Description:
+-- Creates the primary schema used by the Ubuntu SQL
+-- Server project.
+--
+-- Database:
+-- ubuntu_sql_server
+--
+-- Author:
+-- Jonathon Anderson
+--
+-- =====================================================
 
+CREATE SCHEMA IF NOT EXISTS server_management;
+
+COMMENT ON SCHEMA server_management IS
+'Primary schema for server inventory, hardware, networking, operating system, and maintenance records.';
