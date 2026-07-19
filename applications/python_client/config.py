1844ba5b6b52e78b66242181b939a3339e7a93fa
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-19 17:02:01 -0500

    updated config.py

--- applications/python_client/config.py
+++ applications/python_client/config.py
@@ -6,7 +6,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 DATABASE_CONFIG = {
     "host": os.getenv("DB_HOST"),
