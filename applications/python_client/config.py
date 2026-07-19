0eacff7397f2a579ddc8f36c480affad96a1945f
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-19 17:09:08 -0500

    debugged config.py. misplaced "W" on PASSWWORD

--- applications/python_client/config.py
+++ applications/python_client/config.py
@@ -14,6 +14,6 @@ DATABASE_CONFIG = {
     "host": os.getenv("DB_HOST"),
     "database": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSORD"),
+    "password": os.getenv("DB_PASSWORD"),
     "port": int(os.getenv("DB_PORT", 5432))
 }
