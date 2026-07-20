56c7fefafd679ffe1beadb53fdebafb666aa2a64
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-19 21:08:03 -0500

    debugging main.py

--- applications/python_client/main.py
+++ applications/python_client/main.py
@@ -89,7 +89,7 @@ def display_maintenance():
 
     servers = get_servers()
 
-    for server in servers():
+    for server in servers:
 
         print("-" * 60)
         print(f"Maintenance Logs for {server[1]}")
