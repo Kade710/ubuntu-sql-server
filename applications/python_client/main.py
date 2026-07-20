d89aaddfd9933322b146be0770d87eb743d30451
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-19 20:47:11 -0500

    debugging main.py

--- applications/python_client/main.py
+++ applications/python_client/main.py
@@ -67,7 +67,7 @@ def display_network():
 
     servers = get_servers()
 
-    for server in servers():
+    for server in servers:
 
         print("-" * 60)
         print(f"Network Interfaces for {server[1]}")
