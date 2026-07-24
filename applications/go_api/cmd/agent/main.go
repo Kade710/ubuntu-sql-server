ee0275bc6addd3e546b7fe67656c17d8f806a070
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-07-24 13:33:42 -0500

    creating Go_api

--- applications/go_api/cmd/agent/main.go
+++ applications/go_api/cmd/agent/main.go
@@ -1,1 +1,12 @@
+package main
 
+import "fmt"
+
+func main() {
+    fmt.Println("---------------------------")
+    fmt.Println(" Ubuntu SQL Server Go Agent")
+    fmt.Println("---------------------------")
+    fmt.Println()
+    fmt.Println("Status : Placeholder")
+    fmt.Println("Version: 0.1.0")
+}
