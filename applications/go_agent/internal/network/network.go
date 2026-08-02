3f2b9961536649c64f4fb8a22577dfbe9f9a9493
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-01 21:53:06 -0500

    added network.go

--- applications/go_agent/internal/network/network.go
+++ applications/go_agent/internal/network/network.go
@@ -1,1 +1,96 @@
 package network
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+// Interface contains network information collected from Linux.
+type Interface struct {
+	Name		string
+	MACAddress	string
+	IPAddress	string
+	NetworkType string
+	SpeedMbps	int
+}
+
+//Collect gathers active network interfaces with IPv4 addresses.
+func Collect() ([]Interface, error) {
+	systemInterfaces, err := net.Interfaces()
+	if err != nil {
+		return nill, fmt.Errorf("list network interfaces: %w", err)
+	}
+
+	var collected {}Interface
+
+	for _, iface := range systemInterfaces {
+		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+
+		ipAddress := ipv4Address(iface)
+		if ipAddress == "" {
+			continue
+		}
+
+		collected = append(collected, Interface{
+			Name:		 iface.Name,
+			MACAddress:	 iface.HardwareAddr.String(),
+			IPAddress:	 ipAddress,
+			NetworkType: interfaceType(iface.Name),
+			SpeedMbps:	 interfaceSpeed(iface.Name),
+		})
+	}
+
+	return collected, nil
+}
+
+func ipv4Address (iface net.Interface) string {
+	addresses, err := iface.Addrs()
+	if err != nil {
+		return ""
+	}
+
+	for _, address := range addresses {
+		var ip net.IP
+
+		switch value := address.(type) {
+		case *net.IPNet:
+			ip = value.IP
+		case *net.IPAddr:
+			ip = value.IP
+		}
+
+		ip = ip.To4()
+		if ip != nil && !ip.IsLoopback() {
+			return ip.String()
+		}
+	}
+
+	return ""
+}
+
+func interfaceType(name string) string {
+	wirelessPath := filepath.Join("/sys/class/net", name, "wireless")
+
+	if _, err := os.Stat(wirelessPath); err == nil {
+		return "Wireless"
+	}
+
+	return "Ethernet"
+}
+
+func interfaceSpeed(name string) int {
+	speedPath := filePath.Join("/sys/class/net", name, "speed")
+
+	value, err := os.ReadFile(speedPath)
+	if err != nil {
+		return 0
+	}
+
+	return speed
+}
