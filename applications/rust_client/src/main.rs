7e964b6b11818443b628a311801806f49ca0d05f
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-08 16:42:31 -0500

    updated main.rs and menu.rs

--- applications/rust_client/src/main.rs
+++ applications/rust_client/src/main.rs
@@ -5,8 +5,27 @@ mod menu;
 mod models;
 
 fn main() {
-    println!("------------------------------");
-    println!(" Ubuntu SQL Server Rust Client");
-    println!("------------------------------");
-    println!("Status: Initializing");
+    loop {
+        let choice = menu::show_menu();
+
+        match choice.as_str() {
+            "1" => commands::servers::show_servers(),
+            "2" => println!("Server details cooming soon"),
+            "3" => commands::hardware::show_hardware(),
+            "4" => commands::network::show_network(),
+            "5" => commands::health::show_health(),
+            "6" => commands::maintenance::show_maintenance(),
+
+            "0" => {
+                printfn!("\nSee ya!");
+                break;
+            }
+
+            _ => {
+                println!("\nInvalid selection. Try again.");
+            }
+        }
+
+        println!();
+    }
 }
--- applications/rust_client/src/menu.rs
+++ applications/rust_client/src/menu.rs
@@ -1,1 +1,23 @@
 // Interactive menu logic will live here.
+
+use std::io::{self, Write};
+
+pub fn show_menu() -> String {
+    println!("------------------------------");
+    println!(" Ubuntu SQL Server Rust Client");
+    println!("------------------------------");
+    println!("1. List of servers");
+    println!("2. View Server Details");
+    println!("3. View Hardware");
+    println!("4. View Network");
+    println!("5. View Health History");
+    println!("6. View Maintenance Logs");
+    println!("0. Exit");
+    println!("\nSelect an option: ");
+
+    io::stout()
+        .flush()
+        .expect("Failed to flush stout");
+
+    choice.trim().to_string()
+}
