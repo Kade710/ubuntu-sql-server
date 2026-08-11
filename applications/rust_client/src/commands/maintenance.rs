df212e339f3f9c289ddd55465812ec83546da896
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-11 16:51:21 -0500

    debugging maint.rs

--- applications/rust_client/src/commands/maintenance.rs
+++ applications/rust_client/src/commands/maintenance.rs
@@ -7,7 +7,7 @@ use crate::database;
 
 pub fn show_maintenance() {
     println!("Maintenance Logs");
-    println!("---")
+    println!("---");
 
     print!("Enter Server ID");
     io::stdout()
@@ -56,7 +56,7 @@ pub fn show_maintenance() {
         WHERE server_id = $1
         ORDER BY by created_at DESC
         LIMIT 10
-        ".
+        ",
         &[&server_id],
     ) {
         Ok(rows) => rows,
