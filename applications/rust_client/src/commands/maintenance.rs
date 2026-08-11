4183b53bbea7ec5f0ff6ec656ab5e0df95f9292f
Kade710 <jonathonkanderson3@gmail.com>
2026-08-11 16:57:33 -0500

    Complete Rust health history view

--- applications/rust_client/src/commands/maintenance.rs
+++ applications/rust_client/src/commands/maintenance.rs
@@ -10,9 +10,7 @@ pub fn show_maintenance() {
     println!("---");
 
     print!("Enter Server ID");
-    io::stdout()
-        .flush()
-        .expect("Failed to flush stdout");
+    io::stdout().flush().expect("Failed to flush stdout");
 
     let mut input = String::new();
 
@@ -33,7 +31,7 @@ pub fn show_maintenance() {
         Err(error) => {
             println!("Configuration error: {}", error);
             return;
-        }        
+        }
     };
 
     let mut client = match database::connect(&config) {
