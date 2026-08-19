27af095b9d2abdde7f29ca3ba36cf9045524c3f9
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-19 18:58:49 -0500

    updating models.py

--- applications/web_dashboard/dashboard/models.py
+++ applications/web_dashboard/dashboard/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Server(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     hostname = models.CharField(max_length=100)
     ip_address = models.CharField(max_length=45, null=True, blank=True)
     operating_system = models.CharField(max_length=100, null=True, blank=True)
@@ -66,7 +66,7 @@ class HealthCheck(models.Model):
 class MaintenanceLog(models.Model):
     id = models.IntegerField(primary_key=True)
     server_id = models.IntegerField()
-    action = models.CharField(max_length=255)
+    action = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     performed_by = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(null=True, blank=True)
