d27b0860d9b668ab226b941245f281b3f094a5f4
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-20 08:16:53 -0500

    added ntfy alerts to send to my phone

--- applications/go_agent/internal/alerts/ntfy.go
+++ applications/go_agent/internal/alerts/ntfy.go
@@ -1,1 +1,45 @@
+package alerts
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func Send(topic, title, message string) error {
+	if topic == "" {
+		retun fmt.Errorf("NTRY-TOPIC is not configured")
+	}
+
+	url := "https://ntfy.sh/" + topic
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		url,
+		strings.NewReader(message),
+	)
+	if err != nil {
+		return fmt.Errorf("create ntfy request: %w", err)
+	}
+
+	reeq.Header.Set("Title", title)
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("send ntfy notification: %w", err)
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		body, _ := io.ReadAll(response.Body)
+
+		retun fmt.Errorf(
+			"ntfy returned %s: %s",
+			response.Status,
+			string(body),
+		)
+	}
+
+	return nil
+}
