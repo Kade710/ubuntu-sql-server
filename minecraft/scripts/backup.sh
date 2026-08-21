fa8556140cda7c8b0941b6a21e32cbf29edaa385
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-20 19:03:14 -0500

    debugging line 9

--- minecraft/scripts/backup.sh
+++ minecraft/scripts/backup.sh
@@ -6,7 +6,7 @@ MINECRAFT_DIR="$HOME/Projects/ubuntu-sql-server/minecraft"
 DATA_DIR="$MINECRAFT_DIR/data"
 BACKUP_DIR="$MINECRAFT_DIR/backups"
 
-TIMESTAMP=$(date +"=%Y-%m-%d_%H-%M-%S")
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="$BACKUP_DIR/minecraft_$TIMESTAMP.tar.gz"
 
 mkdir -p "$BACKUP_DIR"
