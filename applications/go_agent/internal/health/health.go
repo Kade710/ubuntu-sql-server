2e8ce9b7c79ea4088be761c1b8595fcc0971f26d
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-27 09:45:26 -0500

    returning test

--- applications/go_agent/internal/health/health.go
+++ applications/go_agent/internal/health/health.go
@@ -146,7 +146,7 @@ func overallStatus(load, memoryPercent, diskPercent float64) string {
 		return "CRITICAL"
 	}
 
-	if load >= 0.01 || memoryPercent >= 75 || diskPercent >= 80 {
+	if load >= 4.0 || memoryPercent >= 75 || diskPercent >= 80 {
 		return "WARNING"
 	}
 
