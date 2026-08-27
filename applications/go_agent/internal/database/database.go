359f0810a3e7f11803d8b7afa619029887e7ce85
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-27 13:45:00 -0500

    debugging line 190

--- applications/go_agent/internal/database/database.go
+++ applications/go_agent/internal/database/database.go
@@ -187,7 +187,7 @@ func AddMaintenanceLog(
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	err := db.QueryRowContent(
+	err := db.QueryRowContext(
 		ctx,
 		query,
 		serverID,
