7c13c373812151cf93c854d598198262692b8746
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-27 13:53:33 -0500

    hardening UpsertOS

--- applications/go_agent/internal/database/database.go
+++ applications/go_agent/internal/database/database.go
@@ -228,7 +228,11 @@ func UpsertOperatingSystem(
 
 	var osID int
 
-	err := db.QueryRow(
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := db.QueryRowContext(
+		ctx,
 		query,
 		serverID,
 		info.Distribution,
