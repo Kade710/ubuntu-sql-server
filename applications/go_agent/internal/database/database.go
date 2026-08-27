46c1f4ff76f1e54db6e94a70ac597b42e3a5b21a
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-27 14:12:26 -0500

    hardening AddHealthCheck

--- applications/go_agent/internal/database/database.go
+++ applications/go_agent/internal/database/database.go
@@ -326,7 +326,11 @@ func AddHealthCheck(
 
 	var healthCheckID int
 
-	err := db.QueryRow(
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := db.QueryRowContext(
+		ctx,
 		query,
 		serverID,
 		status.Load1,
