c9526696db412830ab442aaa1b8f7900df95e089
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-28 14:00:49 -0500

    updating maintenance for hardening

--- applications/python_client/maintenance.py
+++ applications/python_client/maintenance.py
@@ -5,10 +5,17 @@
 
 from database import execute_query
 
+
 def get_maintenance_logs(server_id=1):
     """
     Returns maintenance history for a server.
     """
+
+    if not isinstance(server_id, int) or isinstance(server_id, bool):
+        raise TypeError("server_id must be an integer")
+
+    if server_id <= 0:
+        raise ValueError("server_id must be greater than zero")
 
     query = """
         SELECT
