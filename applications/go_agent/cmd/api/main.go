af196ad6326444c1f720eb96fbc1a0876e6a8b82
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-29 18:08:07 -0500

    added api/main.go

--- applications/go_agent/cmd/api/main.go
+++ applications/go_agent/cmd/api/main.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		if _, err := fmt.Fprintln(w, `{"status":"ok"}`); err != nil {
+			log.Printf("failed to write response: %v", err)
+		}
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Ubuntu SQL Server API listening on :8080")
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
