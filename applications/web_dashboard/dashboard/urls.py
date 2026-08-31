4a5fff446c743cef477576b3ff7a63458d9af3a2
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-31 03:59:46 -0500

    added api user

--- applications/web_dashboard/dashboard/urls.py
+++ applications/web_dashboard/dashboard/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("server/<int:server_id>/", views.server_detail, name="server_detail"),
+    path("api/users/", views.api_user_list, name="api_user_list"),
 ]
