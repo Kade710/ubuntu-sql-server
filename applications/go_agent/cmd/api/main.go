f9dbd23f3eba73954c82296b8f125c9ccbb4fce8
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-30 21:53:52 -0500

    debugged line 191

--- applications/go_agent/cmd/api/main.go
+++ applications/go_agent/cmd/api/main.go
@@ -188,7 +188,7 @@ func main() {
 	})
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if len(r.URL.Path) >= 5 && r.URL.Path[:5] =="/api/" {
+		if len(r.URL.Path) >= 5 && r.URL.Path[:5] == "/api/" {
 			requireBearerToken(cfg.APIToken, mux).ServeHTTP(w, r,)
 			return
 		}
