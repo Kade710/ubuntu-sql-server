a5c7f0869795dbb04ab7336b358122d9ba2f0987
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-08-31 22:51:11 -0500

    updated urls

--- applications/web_dashboard/dashboard/urls.py
+++ applications/web_dashboard/dashboard/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("server/<int:server_id>/", views.server_detail, name="server_detail"),
     path("api/users/", views.api_user_list, name="api_user_list"),
     path("api/users/create/", views.api_user_create, name="api_user_create"),
+    path(
+        "api/users/<int:user_id>/disable/",
+        views.api_user_disable,
+        name="api_user_disable",
+    ),
 ]
