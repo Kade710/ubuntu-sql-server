6d1b0bee509c72f013d561b3303dc1f67b212c55
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-01 23:42:40 -0500

    added operator permission

--- applications/web_dashboard/dashboard/tests.py
+++ applications/web_dashboard/dashboard/tests.py
@@ -395,3 +395,28 @@ class RBACRolePermissionTests(TestCase):
             user.has_perm("dashboard.manage_servers")
         )
         
+    def test_operator_has_expected_permissions(self):
+        User = get_user_model()
+
+        user = User.objects.create_user(
+            username="operator",
+            password="TestPassword123!",
+        )
+
+        user.groups.add(
+            user.groups.model.objects.get(
+                name="Operator"
+            )
+        )
+
+        self.assertFalse(
+            user.has_perm("dashboard.manage_users")
+        )
+
+        self.assertTrue(
+            user.has_perm("dashboard.manage_ssh_access")
+        )
+
+        self.assertTrue(
+            user.has_perm("dashboard.manage_servers")
+        )
