2e82c7328781527cbb6490d2ebab95daee25d5e7
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-02 00:21:13 -0500

    updated urls

--- applications/web_dashboard/dashboard/urls.py
+++ applications/web_dashboard/dashboard/urls.py
@@ -2,11 +2,52 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("server/<int:server_id>/", views.server_detail, name="server_detail"),
-    path("api/users/", views.api_user_list, name="api_user_list"),
-    path("api/users/create/", views.api_user_create, name="api_user_create"),
+    path(
+        "",
+        views.index,
+        name="index",
+    ),
+    path(
+        "server/<int:server_id>/",
+        views.server_detail,
+        name="server_detail",
+    ),
+
+    # User management dashboard
+    path(
+        "users/",
+        views.user_management,
+        name="user_management",
+    ),
+    path(
+        "users/create/",
+        views.user_management_create,
+        name="user_management_create",
+    ),
+    path(
+        "users/<int:user_id>/disable/",
+        views.user_management_disable,
+        name="user_management_disable",
+    ),
+    path(
+        "users/<int:user_id>/role/",
+        views.user_management_role,
+        name="user_management_role",
+    ),
+
+    # User management API
+    path(
+        "api/users/",
+        views.api_user_list,
+        name="api_user_list",
+    ),
+    path(
+        "api/users/create/",
+        views.api_user_create,
+        name="api_user_create",
+    ),
     path(
         "api/users/<int:user_id>/disable/",
         views.api_user_disable,
@@ -16,10 +57,5 @@ urlpatterns = [
         "api/users/<int:user_id>/role/",
         views.api_user_role,
         name="api_user_role",
-    ),
-    path(
-        "users/",
-        views.user_management,
-        name="user_management",
     ),
 ]
