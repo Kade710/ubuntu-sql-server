fcce1d3afa524328495dea075157ba9066e59291
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-01 22:07:52 -0500

    updated 0002 rbac

--- applications/web_dashboard/dashboard/migrations/0002_rbac_roles.py
+++ applications/web_dashboard/dashboard/migrations/0002_rbac_roles.py
@@ -1,9 +1,52 @@
 from django.db import migrations
 
+
+ROLES = {
+    "Administrator": [
+        "manage_users",
+        "manage_ssh_access",
+        "manage_servers",
+    ],
+    "Operator": [
+        "manage_ssh_access",
+        "manage_servers",
+    ],
+    "Viewer": [],
+}
+
+
+def create_rbac_roles(apps, schema_editor):
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+
+    for role_name, permission_codenames in ROLES.items():
+        group, _ = Group.objects.get_or_create(name=role_name)
+
+        permissions = Permission.objects.filter(
+            content_type__app_label="dashboard",
+            codename__in=permission_codenames,
+        )
+
+        group.permissions.set(permissions)
+
+
+def remove_rbac_roles(apps, schema_editor):
+    Group = apps.get_model("auth", "Group")
+
+    Group.objects.filter(
+        name__in=ROLES.keys(),
+    ).delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ("dashboard", "0001_rbac_permissions"),
     ]
 
-    operations = []
+    operations = [
+        migrations.RunPython(
+            create_rbac_roles,
+            remove_rbac_roles,
+        ),
+    ]
