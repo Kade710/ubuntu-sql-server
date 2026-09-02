ada215654baa413008fd4ad3a1cf94fea9b81cf4
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-02 16:16:50 -0500

    add sshkey registry

--- applications/web_dashboard/dashboard/forms.py
+++ applications/web_dashboard/dashboard/forms.py
@@ -12,3 +12,19 @@ class MaintenanceLogForm(forms.Form):
         max_length=100,
         required=False
     )
+
+class SSHKeyRegistrationForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        label="Key Name",
+    )
+
+    public_key = forms.CharField(
+        label="OpenSSH Public Key",
+        widget=forms.Textarea(
+            attrs={
+                "rows": 5,
+                "placeholder": "ssh-ed25519 AAAA... user@computer",
+            }
+        ),
+    )
