c56159c58aaf77b9ee676fbf04bcbe5f357fec30
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-03 12:02:39 -0500

    debugged line 5

--- applications/web_dashboard/dashboard/tests.py
+++ applications/web_dashboard/dashboard/tests.py
@@ -2,7 +2,7 @@ import subprocess
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import group, Permission
+from django.contrib.auth.models import Group, Permission
 from django.test import TestCase
 from django.urls import reverse
 
