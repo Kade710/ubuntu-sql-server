18c1277e2de2928230f40bbe6b2f2fab34de6b1b
jonathonkanderson3@gmail.com <jonathonkanderson3@gmail.com>
2026-09-03 14:53:27 -0500

    updated settings

--- applications/web_dashboard/web_dashboard/settings.py
+++ applications/web_dashboard/web_dashboard/settings.py
@@ -21,15 +21,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ikrj=*_8i%q5v670o_mmgr0ql9z00+eri$gzv%5&*vguhz*in'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get(
+    "DJANGO_DEBUG",
+    "False",
+).lower() == "true"
 
 ALLOWED_HOSTS = [
-    '192.168.1.100',
-    'localhost',
-    '127.0.0.1',
+    host.strip()
+    for host in os.environ.get(
+        "DJANGO_ALLOWED_HOSTS",
+        "127.0.0.1,localhost",
+    ).split(",")
+    if host.strip()
 ]
 
 
@@ -124,7 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_URL = BASE_DIR / "staticfiles"
 
 
 # Email
